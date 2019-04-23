3913b84606b47b296800673c1434f5dfce05273d
EnergyCC <sacasti.vlad@gmail.com>
2019-04-23 13:23:15 +0300

    added temporary handlebars templates

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const mysql = require('mysql');
 const connection = require('./db.js');
 const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const app = express();
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//set static
+app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static('html'));
 // connection.db.connect(err => {
 //   if (err) {
@@ -16,9 +24,10 @@ const app = express();
 //Get index route
 
 app.get('/', (req, res) => {
-  res.send('Index file(temp');
+  res.send('Index file(temp)');
 });
 
+//index routes
 app.use('/index', require('./routes/index'));
 
 app.listen(connection.PORT, () => {
--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,37 @@ const router = express.Router();
 const mysql = require('mysql');
 const connection = require('../db');
 
+//database connection
+
+connection.db.connect(err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Connection successfully made');
+  }
+});
+
 //create router for index
 
 router.get('/', (req, res) => {
-  res.send('TEST');
+  let sql = `SELECT * FROM profile;`;
+  connection.db.query(sql, (err, results) => {
+    if (err) {
+      console.log(err);
+      res.sendStatus(403);
+    }
+    // console.log(results);
+    res.render('results', {
+      results
+    });
+  });
+});
+
+router.get('/getdb', (req, res) => {
+  let sql = 'SELECT * FROM profile;';
+  connection.db.query(sql, (err, results) => {
+    res.send(results);
+  });
 });
 
 module.exports = router;
