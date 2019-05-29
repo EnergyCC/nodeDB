47b559dcbb00fed2dc9f594b02eb128bc5db13a6
EnergyCC <sacasti.vlad@gmail.com>
2019-05-29 14:56:05 +0300

    stable version with minor modifiers

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,20 @@
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('http://localhost:3003/service-worker.js')
-            .then(() => {
-                console.log('SW registered');
-            })
-            .catch(console.error);
-    }
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('https://ionut.eccsv.com/service-worker.js')
+    .then(() => {
+      console.log('SW registered');
+    })
+    .catch(console.error);
+}
+
+function genRap(quality = 3) {
+  let doc = new jsPDF('p', 'mm', 'a4');
+  let canvas = document.querySelector('#canvas');
+  let options = {
+    pagesplit: true
+  };
+  console.log(canvas);
+  doc.addHTML(canvas, 0, 0, () => {
+    doc.save('test.pdf');
+  });
+}
